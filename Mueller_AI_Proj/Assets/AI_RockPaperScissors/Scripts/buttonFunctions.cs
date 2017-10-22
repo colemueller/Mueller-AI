54e94577266a92b3cff4f59ab9e04e80ee04a19b
Cole <coleaaronmueller@gmail.com>
2017-10-22 14:26:43 -0400

    Set Up Scene Menu

---
+++ Mueller_AI_Proj/Assets/AI_RockPaperScissors/Scripts/buttonFunctions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonFunctions : MonoBehaviour {
+
+	public void OnExitClick()
+    {
+        Application.Quit();
+    }
+
+    public void OnRockClick()
+    {
+
+    }
+
+    public void OnPaperClick()
+    {
+
+    }
+
+    public void OnScissorsClick()
+    {
+
+    }
+
+    
+}
---
+++ Mueller_AI_Proj/Assets/AI_RockPaperScissors/Scripts/gamemanager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gamemanager : MonoBehaviour {
+
+    public static int trialNum = 0;
+    public int timeValue = 15;
+    public Text timerText;
+    public Text trialText;
+    public Text PlayerWinText;
+    public Text AIWinText;
+
+
+    public void OnStartClick()
+    {
+        trialNum++;
+        timeValue = 15;
+        StartCoroutine(CountDown());
+    }
+
+    IEnumerator CountDown()
+    {
+        yield return new WaitForSeconds(1);
+        timeValue--;
+        StartCoroutine(CountDown());
+    }
+
+    private void Update()
+    {
+        trialText.text = "Trial #" + trialNum.ToString();
+        timerText.text = timeValue.ToString();
+        if(timeValue <= 0)
+        {
+            StopAllCoroutines();
+        }
+    }
+}
