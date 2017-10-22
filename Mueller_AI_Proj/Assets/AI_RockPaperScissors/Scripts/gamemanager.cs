e653b60b81d5794cdf5995af02f53a39615bb69e
Cole <coleaaronmueller@gmail.com>
2017-10-22 18:07:20 -0400

    Made linear/psuedo-random descision tree
    Player can now play R/P/S against a very basic AI that assigns a frequency to the players choices.

--- Mueller_AI_Proj/Assets/AI_RockPaperScissors/Scripts/gamemanager.cs
+++ Mueller_AI_Proj/Assets/AI_RockPaperScissors/Scripts/gamemanager.cs
@@ -15,7 +15,7 @@ public class gamemanager : MonoBehaviour {
     public int scissorFreq = 0;
 
     public static int trialNum = 0;
-    public int timeValue = 15;
+    public int timeValue = 10;
     public GameObject btnGroup;
     public Text timerText;
     public Text trialText;
@@ -23,14 +23,21 @@ public class gamemanager : MonoBehaviour {
     public Text AIWinText;
     public Text PlayerSelectionText;
     public Text AISelectionText;
+    public GameObject TieText;
+    public GameObject StartButton;
 
+    private int PlayerWins = 0;
+    private int AIWins = 0;
 
     public void OnStartClick()
     {
+        TieText.SetActive(false);
         btnGroup.SetActive(true);
         trialNum++;
-        timeValue = 15;
+        timeValue = 10;
         StartCoroutine(CountDown());
+        AISelectionText.text = "";
+        StartButton.SetActive(false);
     }
 
     IEnumerator CountDown()
@@ -46,9 +53,11 @@ public class gamemanager : MonoBehaviour {
         timerText.text = timeValue.ToString();
         if (timeValue <= 0)
         {
+            timeValue = 10;
             btnGroup.SetActive(false);
             StopAllCoroutines();
             MakeAIChoice();
+            StartButton.SetActive(true);
         }
 
         switch (playerChoice)
@@ -70,7 +79,192 @@ public class gamemanager : MonoBehaviour {
 
     public void MakeAIChoice()
     {
+        //====================================================================================================================
+        //EVALUATION -- LINEAR
+        //====================================================================================================================
 
+
+        //~~~~~~~~~~~~~~ ONE CHOICE ~~~~~~~~~~~~~~~
+        //rock most frequent
+        if(rockFreq > paperFreq && rockFreq > scissorFreq)
+        {
+            AIChoice = Type.PAPER;
+        }
+
+        //paper most frequent
+        else if (paperFreq > rockFreq && paperFreq > scissorFreq)
+        {
+            AIChoice = Type.SCISSORS;
+        }
+
+        //scissor most frequent
+        else if (scissorFreq > paperFreq && scissorFreq > rockFreq)
+        {
+            AIChoice = Type.ROCK;
+        }
+
+        //~~~~~~~~~~~~~~ ONE CHOICE END ~~~~~~~~~~~~~~~
+
+
+
+        //rock and paper are same
+        else if (rockFreq == paperFreq && rockFreq > scissorFreq)
+        {
+            int temp = Random.Range(1, 2);
+            if (temp == 1)
+            {
+                AIChoice = Type.PAPER;
+            }
+            else
+            {
+                AIChoice = Type.SCISSORS;
+            }
+        }
+
+        //rock and scissors are same
+        else if (rockFreq == scissorFreq && rockFreq > paperFreq)
+        {
+            int temp = Random.Range(1, 2);
+            if (temp == 1)
+            {
+                AIChoice = Type.PAPER;
+            }
+            else
+            {
+                AIChoice = Type.ROCK;
+            }
+        }
+
+        //paper and scissors are same
+        else if (paperFreq == scissorFreq && paperFreq > rockFreq)
+        {
+            int temp = Random.Range(1, 2);
+            if (temp == 1)
+            {
+                AIChoice = Type.SCISSORS;
+            }
+            else
+            {
+                AIChoice = Type.ROCK;
+            }
+        }
+
+
+
+        //everything is same frequency
+        else
+        {
+            int temp = Random.Range(1, 3);
+            if (temp == 1)
+            {
+                AIChoice = Type.SCISSORS;
+            }
+            else if (temp == 2)
+            {
+                AIChoice = Type.ROCK;
+            }
+            else
+            {
+                AIChoice = Type.PAPER;
+            }
+        }
+
+        //====================================================================================================================
+        //EVALUATION -- LINEAR -- END
+        //====================================================================================================================
+
+
+
+        //change text of AI choice
+        switch (AIChoice)
+        {
+            case Type.ROCK:
+                AISelectionText.text = "ROCK";
+                break;
+
+            case Type.PAPER:
+                AISelectionText.text = "PAPER";
+                break;
+
+            case Type.SCISSORS:
+                AISelectionText.text = "SCISSORS";
+                break;
+        }
+
+        //AT END -- learn what player picked, increase its frequency
+        switch (playerChoice)
+        {
+            case Type.ROCK:
+                rockFreq++;
+                break;
+
+            case Type.PAPER:
+                paperFreq++;
+                break;
+
+            case Type.SCISSORS:
+                scissorFreq++;
+                break;
+        }
+
+        ChooseWinner();
+    }
+
+    public void ChooseWinner()
+    {
+        switch (playerChoice)
+        {
+            case Type.ROCK:
+                if(AIChoice == Type.PAPER)
+                {
+                    AIWins++;
+                    AIWinText.text = "Wins:" + AIWins.ToString();
+                }
+                else if (AIChoice == Type.SCISSORS)
+                {
+                    PlayerWins++;
+                    PlayerWinText.text = "Wins:" + PlayerWins.ToString();
+                }
+                else
+                {
+                    TieText.SetActive(true);
+                }
+                break;
+
+            case Type.PAPER:
+                if (AIChoice == Type.SCISSORS)
+                {
+                    AIWins++;
+                    AIWinText.text = "Wins:" + AIWins.ToString();
+                }
+                else if (AIChoice == Type.ROCK)
+                {
+                    PlayerWins++;
+                    PlayerWinText.text = "Wins:" + PlayerWins.ToString();
+                }
+                else
+                {
+                    TieText.SetActive(true);
+                }
+                break;
+
+            case Type.SCISSORS:
+                if (AIChoice == Type.ROCK)
+                {
+                    AIWins++;
+                    AIWinText.text = "Wins:" + AIWins.ToString();
+                }
+                else if (AIChoice == Type.PAPER)
+                {
+                    PlayerWins++;
+                    PlayerWinText.text = "Wins:" + PlayerWins.ToString();
+                }
+                else
+                {
+                    TieText.SetActive(true);
+                }
+                break;
+        }
     }
 
     //===============================================
